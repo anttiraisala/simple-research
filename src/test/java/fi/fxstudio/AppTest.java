9a9e341d133959b2cd5ba781447fbbcace3ef9f4
Antti Räisälä <antti.raisala@vismaconsulting.fi>
2017-09-10 22:49:59 +0300

    First files

---
+++ src/test/java/fi/fxstudio/AppTest.java
@@ -0,0 +1,17 @@
+package fi.fxstudio;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void dummyTest(){
+        System.out.println("Dummy test case");
+        assertTrue(true);
+    }
+
+}
